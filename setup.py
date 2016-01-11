638e9826a514287cce1a676121989c32f89138aa
Maarten Breddels <maartenbreddels@gmail.com>
2016-01-11 14:59:28 +0100

    proper prefix
    when installing as non-user, use the proper prefix instead of /usr/

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 import os
 from notebook.nbextensions import install_nbextension
 from notebook.services.config import ConfigManager
-
+import sys
 livereveal_dir = os.path.join(os.path.dirname(__file__), 'livereveal')
 
-def install(use_symlink=False, enable=True):
+def install(use_symlink=False, enable=True, user=False):
     # Install the livereveal code.
     install_nbextension(livereveal_dir, symlink=use_symlink,
-                        overwrite=use_symlink, user=True)
+                        overwrite=use_symlink, user=user,
+			prefix=sys.prefix if not user else None)
 
     if enable:
         cm = ConfigManager()
@@ -25,13 +26,15 @@ def main():
     install_parser = subparsers.add_parser('install')
     install_parser.add_argument('--develop', action='store_true',
                                 help="Install livereveal  as a symlink to the source.")
+    install_parser.add_argument('--user', action='store_true',
+                                help="Install as user")
     install_parser.add_argument('--no-enable', action='store_true',
                                 help="Install but don't enable the extension.")
 
     args = parser.parse_args(sys.argv[1:])
 
     install(use_symlink=args.develop,
-            enable=(not args.no_enable))
+            enable=(not args.no_enable), user=args.user)
 
 
 if __name__ == '__main__':
